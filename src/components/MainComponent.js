68675290e6a18fe42d88551c37f2b8e74bfd3ca5
Josephine Mensching <josiemensching72@gmail.com>
2022-04-14 15:48:07 -0400

    Week 3 React Project

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
+import About from './AboutComponent';
 
 class Main extends Component {
     constructor(props) {
@@ -48,11 +49,12 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
-                <Directory campsites={this.state.campsites} />
+                
                   
              
                 <Footer />
